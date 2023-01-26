0001f414c2e0f56f7841e2a2a77715bc8d7b7b52
Robbendinho <r.vanbezouwen@bluetrail.nl>
2023-01-26 16:03:55 +0100

    Parallel testing in testrunner

--- BlueTrailWorld/src/test/java/APIdemoTests/TestRunner.java
+++ BlueTrailWorld/src/test/java/APIdemoTests/TestRunner.java
@@ -1,12 +1,16 @@
 package APIdemoTests;
 
-import com.intuit.karate.junit5.Karate;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class TestRunner {
 	
-	@Karate.Test
-    Karate testAll() {
-        return Karate.run().relativeTo(getClass());
+    @Test
+    public void testParallel() {
+        Results results = Runner.path("classpath:APIdemoTests").tags("~@skipme").parallel(6);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
 }
 
